ceb7422607554777fa836536aec2f79f631a6ddb
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2024-01-15 07:48:16 +0000

    eddited css

--- senior-project/src/pages/belligol.jsx
+++ senior-project/src/pages/belligol.jsx
@@ -6,9 +6,23 @@ import '../styles/Belligol.css'
 function belligol() {
   return (
     <div className='Belli'>
+
         <header id='belliHead'>
             <h1>Belligol</h1>
         </header>
+
+        <Navbar />
+
+        <section id='belliContent'>
+          <h2>Yo</h2>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </section>
+
+        <footer id='foot'>
+            Senior Project 2024 &copy;
+        </footer>
     </div>
     
 
