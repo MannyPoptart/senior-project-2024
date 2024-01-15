1e29ad4308fca9c37b17e562d539118eec299339
Omar Facundo <114097196+omar5o9@users.noreply.github.com>
2024-01-15 08:19:21 +0000

    Added more styling

--- senior-project/src/pages/belligol.jsx
+++ senior-project/src/pages/belligol.jsx
@@ -14,10 +14,17 @@ function belligol() {
         <Navbar />
 
         <section id='belliContent'>
-          <h2>Yo</h2>
+          <h2>Belligol</h2>
+        
+          <iframe width="560" height="560" src="https://www.youtube.com/embed/s6CnJcdRQkU?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
           <p>
-            lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          BELLIGOOOOOOOOOOOOOOL
+          BELLIGOL
+          BELLINGHAM, E ELE 
+          O ILUMINADO, DO BELLINGHAM
           </p>
+          
+          <img src="https://i.kym-cdn.com/photos/images/newsfeed/002/691/290/48e.jpg" alt="belligol" />
         </section>
 
         <footer id='foot'>
